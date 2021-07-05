4587001a44a901fdb0c06b74665ab4b9307ac927
soduma <soduma@naver.com>
2021-07-05 16:45:49 +0900

    NUMBERS

---
+++ Pods/Target Support Files/Pods-myRxSwift/Pods-myRxSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myRxSwift : NSObject
+@end
+@implementation PodsDummy_Pods_myRxSwift
+@end
--- myRxSwift/ViewController.swift
+++ myRxSwift/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Observable.combineLatest(textField1.rx.text.orEmpty, textField2.rx.text.orEmpty, textField3.rx.text.orEmpty) { a, b, c -> Int in
+            return (Int(a) ?? 0 ) + (Int(b) ?? 0 ) + (Int(c) ?? 0 )
+        }
+        .map { $0.description }
+        .bind(to: resultLabel.rx.text)
+        .disposed(by: disposeBag)
     }
-
-
 }
 
